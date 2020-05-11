4a108abaf613a5a6229c4f2cea03bb41ff3bd077
vagrant <vagrant@localhost.localdomain>
2020-05-11 10:01:40 +0000

    .circleci/config.ymlの修正

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,42 +1,42 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
   before do
     @user = User.create(
-	  name: "Anakin",
-	  email: "Anakin@Skaywalker",
-	  password: "AnakinSkywalker",
-	)
+      name: 'Anakin',
+      email: 'Anakin@Skaywalker',
+      password: 'AnakinSkywalker'
+    )
   end
 
-  it "is valid with a name, email, password" do
-	expect(@user).to be_valid
+  it 'is valid with a name, email, password' do
+    expect(@user).to be_valid
   end
 
-  it "is invalid without name" do
-	user = FactoryBot.build(:user, name: nil)
-	user.valid?
-	expect(user.errors[:name]).to include("can't be blank")
+  it 'is invalid without name' do
+    user = FactoryBot.build(:user, name: nil)
+    user.valid?
+    expect(user.errors[:name]).to include("can't be blank")
   end
 
-  it "is invalid without e-mail" do
+  it 'is invalid without e-mail' do
     user = FactoryBot.build(:user, email: nil)
-	user.valid?
-	expect(user.errors[:email]).to include("can't be blank")
+    user.valid?
+    expect(user.errors[:email]).to include("can't be blank")
   end
 
-  it "is invalid without password" do
+  it 'is invalid without password' do
     user = FactoryBot.build(:user, password: nil)
-	user.valid?
-	expect(user.errors[:password]).to include("can't be blank")
+    user.valid?
+    expect(user.errors[:password]).to include("can't be blank")
   end
 
-  it "is invalid with a duplicate email address" do
-    FactoryBot.create(:user, email: "tester@tester")
-	user = FactoryBot.build(:user, email: "tester@tester")
-	user.valid?
-	expect(user.errors[:email]).to include("has already been taken")
+  it 'is invalid with a duplicate email address' do
+    FactoryBot.create(:user, email: 'tester@tester')
+    user = FactoryBot.build(:user, email: 'tester@tester')
+    user.valid?
+    expect(user.errors[:email]).to include('has already been taken')
   end
-  
-   
 end
