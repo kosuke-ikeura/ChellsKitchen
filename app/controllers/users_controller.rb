f027873e00366820d810e6f71421429bf613dce5
vagrant <vagrant@localhost.localdomain>
2020-05-11 23:46:18 +0000

    users/show.html.erbにpagenation追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@
 
 class UsersController < ApplicationController
   def show
-    @flavors = current_user.flavors
+    @flavors = current_user.flavors.order(id: 'ASC').page(params[:page]).per(5)
   end
 
   def image
