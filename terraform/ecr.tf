a87e372e4fc30e82fe2a024dc7a699181f6959d7
vagrant <vagrant@localhost.localdomain>
2020-05-21 00:56:02 +0000

    terraformの修正

---
+++ terraform/ecr.tf
@@ -0,0 +1,33 @@
+resource "aws_ecr_repository" "ck-ecr-nginx" {
+    name = "ck-ecr-nginx"
+}
+
+resource "aws_ecr_repository" "ck-ecr-rails" {
+    name = "ck-ecr-rails"
+}
+
+
+resource "aws_ecr_lifecycle_policy" "ck-ecr" {
+    repository = aws_ecr_repository.ck-ecr-nginx.name
+
+    policy = <<EOF
+    {
+        "rules": [
+            {
+                "rulePriority": 1,
+                "description": "Keep last 30 release tagged images",
+                "selection": {
+                    "tagStatus": "tagged",
+                    "tagPrefixList": ["release"],
+                    "countType": "imageCountMoreThan",
+                    "countNumber": 30
+                },
+                "action": {
+                    "type": "expire"
+                }
+            }
+        ]
+    }
+EOF
+}
+
